d7e304eca0107ebca456f68777e37a9cd2351b74
Bachir <bachir@Bachirs-MacBook-Pro.local>
2022-11-14 19:48:06 -0500

    added api, script and css stye to cocktail component

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,10 +1,46 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 
 export default function Cocktail() {
+
+    const[data, setData] = useState([]);
+
+    const getData = () => {
+        fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data.response);
+            setData(data.drinks);
+        });
+    };
+
+    useEffect(() => {
+        getData();
+    }, [])
+
     return(
         <div className="drinks">
-            this cocktail
+            <h2 className="cocktail-title">Check your favorite Drinks</h2>
+            <div className="cocktails">
+
+                {data.map(drink => {
+                    return(
+                        <>
+                            <p key={drink.idDrink} className="drink-items">
+                            <img src={drink.strDrinkThumb} alt="img" className="drink-img" />
+                            <p className="text">{drink.strDrink}</p> 
+                            <p className="text">{drink.strCategory}</p>
+                            </p>
+                        </>
+                    )
+                })}
+            </div>
+
+            <button 
+            type="submit" 
+            className="btn-cocktail"
+            onClick={getData}>Get a cocktail</button>
         </div>
     )
 }
